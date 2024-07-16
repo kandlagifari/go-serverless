8310062f30450a3e2e2e89311d2b51fd222e1784
kandlagifari <kandlagifari@gmail.com>
2024-07-16 10:54:26 +0700

    Add Item Movie

---
+++ 08_s3-frontend/frontend/src/app/components/movie-item/movie-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'movie-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-item.component.html',
+  styleUrl: './movie-item.component.css'
+})
+export class MovieItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
